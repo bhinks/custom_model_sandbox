db713583a3bbe01f878ebb65ea703869f6153d92
Brent Hinks <brent.hinks@datarobot.com>
2023-04-04 17:47:12 -0400

    update model fit pipeline

--- deploy/custom.py
+++ deploy/custom.py
@@ -5,7 +5,6 @@ This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
 import pandas as pd
-import numpy as np
 
 
 def transform(data, model):
@@ -27,105 +26,8 @@ def transform(data, model):
     """
     # Execute any steps you need to do before scoring
     # Remove target columns if they're in the dataset
-    df = data.rename(columns={
-        "glyburide.metformin": "glyburide_metformin", 
-        "glipizide.metformin": "glipizide_metformin",
-        "glimepiride.pioglitazone": "glimepiride_pioglitazone",
-        "metformin.rosiglitazone": "metformin_rosiglitazone",
-        "metformin.pioglitazone": "metformin_pioglitazone"
-    })
-    
-    for target_col in ["readmitted", "diag_1", "diag_2", "diag_3", "diag_1_desc", "diag_2_desc", "diag_3_desc"]:
-        if target_col in df:
-            df.pop(target_col)
-    df = df.fillna(0)
-
-    df.replace("None", np.nan, inplace=True)
-    df.replace("No", 0, inplace=True)
-    df.replace("Yes", 1, inplace=True)
-
-    df.race = pd.Categorical(df.race)
-    df["race_cat"] = df.race.cat.codes
-
-    df.gender = pd.Categorical(df.gender)
-    df["gender_cat"] = df.gender.cat.codes
-
-    df.age = pd.Categorical(df.age)
-    df["age_cat"] = df.age.cat.codes
-
-    df.weight = pd.Categorical(df.weight)
-    df["weight_cat"] = df.weight.cat.codes
-
-    df.admission_type_id = pd.Categorical(df.admission_type_id)
-    df["admission_type_id_cat"] = df.admission_type_id.cat.codes
-
-    df.discharge_disposition_id = pd.Categorical(df.discharge_disposition_id)
-    df["discharge_disposition_id_cat"] = df.discharge_disposition_id.cat.codes
-
-    df.admission_source_id = pd.Categorical(df.admission_source_id)
-    df["admission_source_id_cat"] = df.admission_source_id.cat.codes
-
-    df.payer_code = pd.Categorical(df.payer_code)
-    df["payer_code_cat"] = df.payer_code.cat.codes
-
-    df.medical_specialty = pd.Categorical(df.medical_specialty)
-    df["medical_specialty_cat"] = df.medical_specialty.cat.codes
-
-    df.A1Cresult = pd.Categorical(df.A1Cresult)
-    df["A1Cresult_cat"] = df.A1Cresult.cat.codes
-
-    df.insulin = pd.Categorical(df.insulin)
-    df["insulin_cat"] = df.insulin.cat.codes
-
-    df.change = pd.Categorical(df.change)
-    df["change_cat"] = df.change.cat.codes
-
-    df.max_glu_serum = pd.Categorical(df.max_glu_serum)
-    df["max_glu_serum_cat"] = df.max_glu_serum.cat.codes
-
-    df.metformin = pd.Categorical(df.metformin)
-    df["metformin_cat"] = df.metformin.cat.codes
-
-    df.repaglinide = pd.Categorical(df.repaglinide)
-    df["repaglinide_cat"] = df.repaglinide.cat.codes
-
-    df.nateglinide = pd.Categorical(df.nateglinide)
-    df["nateglinide_cat"] = df.nateglinide.cat.codes
-
-    df.chlorpropamide = pd.Categorical(df.chlorpropamide)
-    df["chlorpropamide_cat"] = df.chlorpropamide.cat.codes
-
-    df.glimepiride = pd.Categorical(df.glimepiride)
-    df["glimepiride_cat"] = df.glimepiride.cat.codes
-
-    df.glipizide = pd.Categorical(df.glipizide)
-    df["glipizide_cat"] = df.glipizide.cat.codes
-
-    df.glyburide = pd.Categorical(df.glyburide)
-    df["glyburide_cat"] = df.glyburide.cat.codes
-
-    df.tolbutamide = pd.Categorical(df.tolbutamide)
-    df["tolbutamide_cat"] = df.tolbutamide.cat.codes
-
-    df.pioglitazone = pd.Categorical(df.pioglitazone)
-    df["pioglitazone_cat"] = df.pioglitazone.cat.codes
-
-    df.rosiglitazone = pd.Categorical(df.rosiglitazone)
-    df["rosiglitazone_cat"] = df.rosiglitazone.cat.codes
-
-    df.acarbose = pd.Categorical(df.acarbose)
-    df["acarbose_cat"] = df.acarbose.cat.codes
-
-    df.miglitol = pd.Categorical(df.miglitol)
-    df["miglitol_cat"] = df.miglitol.cat.codes
-
-    df.tolazamide = pd.Categorical(df.tolazamide)
-    df["tolazamide_cat"] = df.tolazamide.cat.codes
-
-    df.glyburide_metformin = pd.Categorical(df.glyburide_metformin)
-    df["glyburide_metformin_cat"] = df.glyburide_metformin.cat.codes
-
-    df.glipizide_metformin = pd.Categorical(df.glipizide_metformin)
-    df["glipizide_metformin_cat"] = df.glipizide_metformin.cat.codes
-
-    return df
+    for target_col in ["readmitted"]:
+        if target_col in data:
+            data.pop(target_col)
+    data = data.fillna(0)
+    return data
--- train_model.py
+++ train_model.py
@@ -2,124 +2,36 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.compose import make_column_transformer
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 df = pd.read_csv("data/10kDiabetes.csv")
 
-df = df.rename(columns={
-    "glyburide.metformin": "glyburide_metformin", 
-    "glipizide.metformin": "glipizide_metformin",
-    "glimepiride.pioglitazone": "glimepiride_pioglitazone",
-    "metformin.rosiglitazone": "metformin_rosiglitazone",
-    "metformin.pioglitazone": "metformin_pioglitazone"
-})
-
-df.replace("None", np.nan, inplace=True)
-df.replace("No", 0, inplace=True)
-df.replace("Yes", 1, inplace=True)
-
-df.race = pd.Categorical(df.race)
-df["race_cat"] = df.race.cat.codes
-
-df.gender = pd.Categorical(df.gender)
-df["gender_cat"] = df.gender.cat.codes
-
-df.age = pd.Categorical(df.age)
-df["age_cat"] = df.age.cat.codes
-
-df.weight = pd.Categorical(df.weight)
-df["weight_cat"] = df.weight.cat.codes
-
-df.admission_type_id = pd.Categorical(df.admission_type_id)
-df["admission_type_id_cat"] = df.admission_type_id.cat.codes
-
-df.discharge_disposition_id = pd.Categorical(df.discharge_disposition_id)
-df["discharge_disposition_id_cat"] = df.discharge_disposition_id.cat.codes
-
-df.admission_source_id = pd.Categorical(df.admission_source_id)
-df["admission_source_id_cat"] = df.admission_source_id.cat.codes
-
-df.payer_code = pd.Categorical(df.payer_code)
-df["payer_code_cat"] = df.payer_code.cat.codes
-
-df.medical_specialty = pd.Categorical(df.medical_specialty)
-df["medical_specialty_cat"] = df.medical_specialty.cat.codes
-
-df.A1Cresult = pd.Categorical(df.A1Cresult)
-df["A1Cresult_cat"] = df.A1Cresult.cat.codes
-
-df.insulin = pd.Categorical(df.insulin)
-df["insulin_cat"] = df.insulin.cat.codes
-
-df.change = pd.Categorical(df.change)
-df["change_cat"] = df.change.cat.codes
-
-df.max_glu_serum = pd.Categorical(df.max_glu_serum)
-df["max_glu_serum_cat"] = df.max_glu_serum.cat.codes
-
-df.metformin = pd.Categorical(df.metformin)
-df["metformin_cat"] = df.metformin.cat.codes
-
-df.repaglinide = pd.Categorical(df.repaglinide)
-df["repaglinide_cat"] = df.repaglinide.cat.codes
-
-df.nateglinide = pd.Categorical(df.nateglinide)
-df["nateglinide_cat"] = df.nateglinide.cat.codes
-
-df.chlorpropamide = pd.Categorical(df.chlorpropamide)
-df["chlorpropamide_cat"] = df.chlorpropamide.cat.codes
-
-df.glimepiride = pd.Categorical(df.glimepiride)
-df["glimepiride_cat"] = df.glimepiride.cat.codes
-
-df.glipizide = pd.Categorical(df.glipizide)
-df["glipizide_cat"] = df.glipizide.cat.codes
-
-df.glyburide = pd.Categorical(df.glyburide)
-df["glyburide_cat"] = df.glyburide.cat.codes
-
-df.tolbutamide = pd.Categorical(df.tolbutamide)
-df["tolbutamide_cat"] = df.tolbutamide.cat.codes
-
-df.pioglitazone = pd.Categorical(df.pioglitazone)
-df["pioglitazone_cat"] = df.pioglitazone.cat.codes
-
-df.rosiglitazone = pd.Categorical(df.rosiglitazone)
-df["rosiglitazone_cat"] = df.rosiglitazone.cat.codes
-
-df.acarbose = pd.Categorical(df.acarbose)
-df["acarbose_cat"] = df.acarbose.cat.codes
-
-df.miglitol = pd.Categorical(df.miglitol)
-df["miglitol_cat"] = df.miglitol.cat.codes
-
-df.tolazamide = pd.Categorical(df.tolazamide)
-df["tolazamide_cat"] = df.tolazamide.cat.codes
-
-df.glyburide_metformin = pd.Categorical(df.glyburide_metformin)
-df["glyburide_metformin_cat"] = df.glyburide_metformin.cat.codes
-
-df.glipizide_metformin = pd.Categorical(df.glipizide_metformin)
-df["glipizide_metformin_cat"] = df.glipizide_metformin.cat.codes
-
-features = [
-    "race_cat", "gender_cat", "age_cat", "weight_cat", "admission_type_id_cat", "discharge_disposition_id_cat", "admission_source_id_cat", 
-    "time_in_hospital", "payer_code_cat", "medical_specialty_cat", "num_lab_procedures", "num_procedures", "num_medications", "number_outpatient",
-    "number_emergency", "number_inpatient", "number_diagnoses", "max_glu_serum_cat", "A1Cresult_cat",
-    "metformin_cat", "repaglinide_cat", "nateglinide_cat", "chlorpropamide_cat", "glimepiride_cat", "acetohexamide", "glipizide_cat", "glyburide_cat",
-    "tolbutamide_cat", "pioglitazone_cat", "rosiglitazone_cat", "acarbose_cat", "miglitol_cat", "troglitazone", "tolazamide_cat", "examide", "citoglipton",
-    "insulin_cat", "glyburide_metformin_cat", "glipizide_metformin_cat", "glimepiride_pioglitazone", "metformin_rosiglitazone", "metformin_pioglitazone",
-    "change_cat", "diabetesMed"
-]
-
-x = np.array(df[features])
-y = np.array(df["readmitted"])
+x = df.drop(["readmitted"], axis=1)
+y = df["readmitted"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1337)
 
-model = LogisticRegression()
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+ohe_features_to_encode = x_train.columns[x_train.dtypes==object].tolist()
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('encoder', OneHotEncoder(handle_unknown='ignore'))])
 
-pickle.dump(model, open("deploy/model.pkl", "wb"))
+sim_num_features_to_encode = list(x_train.select_dtypes(include = ['float64', 'int64']).columns)
+sim_num = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+
+col_trans = make_column_transformer((categorical_transformer, ohe_features_to_encode), (sim_num, sim_num_features_to_encode), remainder = "passthrough")
+
+model = LogisticRegression()
+
+pipe = make_pipeline(col_trans, model)
+pipe.fit(x_train,y_train)
+
+pipe.fit(x_train, y_train)
+
+pickle.dump(pipe, open("deploy/model.pkl", "wb"))
